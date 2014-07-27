0e06df62cdcd8febb4038caa0e563c5032f4e218
vv1133 <zhengdi_vv1133@foxmail.com>
2014-07-27 16:42:10 +0800

    kill subprocess when exit

--- play.py
+++ play.py
@@ -3,18 +3,33 @@
 import random
 import glob
 import os
+import subprocess
+import signal
+
+proc = None
+
+def onsignal_term(a,b):
+	global proc
+	print 'finish'
+	subprocess.Popen.kill(proc)
+	os.exit(0)
+
+signal.signal(signal.SIGTERM, onsignal_term)
+
 
 #path = os.getcwd()
 #print path
 #os.listdir(path)
 
-list = glob.glob("*.mp3")
+
+list = glob.glob("/home/pi/music/*.mp3")
 
 while True:
 	file = random.choice(list)
+	cmd = "mpg123 " + file
 	print "play: %s" %(file)
-	if os.system("mpg123 %s" %(file)) != 0:
-		break
-
-print "finish"
+	args = cmd.split(" ")
+	proc = subprocess.Popen(args)
+	subprocess.Popen.wait(proc)
+	print "next"
 
